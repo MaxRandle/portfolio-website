ff09e71dbe8196bfaa300d4d7d0d4d93e86b75fb
max <max@Maxwells-MacBook-Pro.local>
2019-10-05 00:13:11 +1300

    added links to the github and linkedin icons

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import "./App.scss";
 import HomePage from "./HomePage";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
-	pageContainer: {
-		padding: theme.spacing(4)
-	}
+  pageContainer: {
+    padding: theme.spacing(4)
+  }
 }));
 
 function App() {
-	const classes = useStyles();
-	const [pageIndex, setPageIndex] = useState(0);
-	const page = [<HomePage />][pageIndex];
+  const classes = useStyles();
+  const [pageIndex, setPageIndex] = useState(0);
+  const page = [<HomePage />][pageIndex];
 
-	return (
-		<div className="App">
-			<Container className={classes.pageContainer} maxWidth="md">
-				{page}
-			</Container>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Container className={classes.pageContainer} maxWidth="md">
+        {page}
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/HomePage.js
+++ src/HomePage.js
@@ -10,57 +10,68 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
-	gridContainer: {
-		//padding: theme.spacing(3)
-	},
-	logo: {
-		height: 160,
-		paddingBottom: theme.spacing(4)
-	},
-	gridItem: {
-		padding: theme.spacing(4),
-		textAlign: "center"
-	}
+  gridContainer: {
+    //padding: theme.spacing(3)
+  },
+  logo: {
+    height: 80,
+    paddingBottom: theme.spacing(4)
+  },
+  gridItem: {
+    padding: theme.spacing(4),
+    textAlign: "center"
+  },
+  block: {
+    padding: theme.spacing(4)
+  }
 }));
 
 const HomePage = () => {
-	const classes = useStyles();
-	return (
-		<Box>
-			<Grid container direction="row" justify="flex-start" alignItems="center" className="name first">
-				<Grid item className="ma">
-					MA
-				</Grid>
-				<Grid item className="x">
-					X
-				</Grid>
-			</Grid>
+  const classes = useStyles();
+  return (
+    <Box>
+      <Container className={classes.block}>
+        <Grid container direction="row" justify="flex-start" alignItems="center" className="name first">
+          <Grid item className="ma">
+            MA
+          </Grid>
+          <Grid item className="x">
+            X
+          </Grid>
+        </Grid>
 
-			<Grid container direction="row" justify="flex-start" alignItems="center" className="name last">
-				<Grid item className="r">
-					R
-				</Grid>
-				<Grid item className="andle">
-					ANDLE
-				</Grid>
-			</Grid>
+        <Grid container direction="row" justify="flex-start" alignItems="center" className="name last">
+          <Grid item className="r">
+            R
+          </Grid>
+          <Grid item className="andle">
+            ANDLE
+          </Grid>
+        </Grid>
+      </Container>
 
-			<Grid container direction="row" justify="flex-start" alignItems="center" spacing={6}>
-				<Grid item className="r">
-					<img className={classes.logo} src={github_logo} alt="github logo" />
-				</Grid>
-				<Grid item className="r">
-					<img className={classes.logo} src={linkedin_logo} alt="linkedin logo" />
-				</Grid>
-			</Grid>
-		</Box>
-	);
+      <Container className={classes.block}>
+        <Grid container direction="row" justify="flex-start" alignItems="center" spacing={6}>
+          <Grid item className="r">
+            <a href="https://github.com/MaxRandle" target="_blank">
+              <img className={classes.logo} src={github_logo} alt="github logo" />
+            </a>
+          </Grid>
+          <Grid item className="r">
+            <a href="https://www.linkedin.com/in/max-randle-a79760160/" target="_blank">
+              <img className={classes.logo} src={linkedin_logo} alt="linkedin logo" />
+            </a>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default HomePage;
 
 {
-	/* <Grid container justify="center" alignItems="center" className={classes.gridContainer}>
+  /* <Grid container justify="center" alignItems="center" className={classes.gridContainer}>
 	<Grid item className={classes.gridItem} sm={6} xs={12}>
 		<img className={classes.logo} src={react_logo} alt="react logo" />
 		<Typography variant="h3">React</Typography>
